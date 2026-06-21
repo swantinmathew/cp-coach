12de30461155cc59f0cdbdb1c9f0e6244821a4dc
Swantin63 <swantinmathew@gmail.com>
2026-06-21 23:42:16 +0530

    Styled  landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/landing/Navbar";
 import HeroSection from "../components//landing/HeroSection";
 import FeaturesSection from "../components/landing/FeaturesSection";
 import Footer from "../components/landing/Footer";
+import DashboardShowcase from "../components/landing/DashboardShowcase";
 function LandingPage()
 {
     return (
@@ -9,6 +10,7 @@ function LandingPage()
         <Navbar/>
         <HeroSection/>
         <FeaturesSection/>
+        <DashboardShowcase />
         <Footer/>
         </>
     );
