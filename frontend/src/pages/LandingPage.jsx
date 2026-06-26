637f55546840f5c898d1414b6279671f9aaae25a
Swantin63 <swantinmathew@gmail.com>
2026-06-26 22:19:36 +0530

    restructured landing

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,23 +1,35 @@
 import Navbar from "../components/landing/Navbar";
-import HeroSection from "../components//landing/HeroSection";
+import HeroSection from "../components/landing/HeroSection";
 import FeaturesSection from "../components/landing/FeaturesSection";
-import Footer from "../components/landing/Footer";
-import DashboardShowcase from "../components/landing/DashboardShowcase";
-import HowItWorks from "../components/landing/HowItWorks";
+import RoadmapShowcase from "../components/landing/RoadmapShowcase";
 import AnalyticsSection from "../components/landing/AnalyticsSection";
+import HowItWorks from "../components/landing/HowItWorks";
+import Footer from "../components/landing/Footer";
 
-function LandingPage()
-{
+function LandingPage() {
+
     return (
+
         <>
-        <Navbar/>
-        <HeroSection/>
-        <FeaturesSection/>
-        <AnalyticsSection />
-        <HowItWorks />
-        <Footer/>
+
+            <Navbar />
+
+            <HeroSection />
+
+            <FeaturesSection />
+
+            <RoadmapShowcase />
+
+            <AnalyticsSection />
+
+            <HowItWorks />
+
+            <Footer />
+
         </>
+
     );
+
 }
 
 export default LandingPage;
